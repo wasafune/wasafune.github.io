c9c692132453c25b58f5e457b96ec93e4195862c
wasafune <wasafune@gmail.com>
2018-04-10 18:15:52 -0700

    (add)loading fade animation

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 console.log('expect(earth()).toBe(42)');
 
+const { body } = document;
+const style = window.getComputedStyle(body);
+const bgSrc = style.backgroundImage;
+const bgUrl = bgSrc.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+const img = new Image();
+img.onload = () => {
+  body.setAttribute('class', 'fade-in-element');
+}
+img.src = bgUrl;
+if (img.complete) {
+  img.onload();
+};
+
 const projectLink = document.getElementById('project-scroll');
 const projectContainer = document.getElementById('project-container');
-const bgSrc = document.body.style.backgroundImage;
-console.log(bgSrc);
-
 
 projectLink.addEventListener('click', () => {
   const options = {
