c3e265bf9f257e2df66a9c4818f45a43ff6ba5ef
wasafune <wasafune@gmail.com>
2019-04-13 15:03:12 -0700

    glitch to random ints

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-console.log('expect(earth()).toBe(42);');
+console.log('expect(earth()).toBe(42)')
 
-const { body } = document;
-const style = window.getComputedStyle(body);
-const bgSrc = style.backgroundImage;
-const bgUrl = bgSrc.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
-const img = new Image();
+const { body } = document
+const style = window.getComputedStyle(body)
+const bgSrc = style.backgroundImage
+const bgUrl = bgSrc.match(/\((.*?)\)/)[1].replace(/('|")/g,'')
+const img = new Image()
 img.onload = () => {
-  body.setAttribute('class', 'fade-in-element');
+  body.setAttribute('class', 'fade-in-element')
+  setTimeout(() => {
+    randomInterval(runGlitch, 3600, 16000)
+  }, 2400)
 }
-img.src = bgUrl;
+img.src = bgUrl
 if (img.complete) {
-  img.onload();
-};
+  img.onload()
+}
 
-const projectLink = document.getElementById('project-scroll');
-const projectContainer = document.getElementById('project-container');
+const projectLink = document.getElementById('project-scroll')
+const projectContainer = document.getElementById('project-container')
 
 projectLink.addEventListener('click', () => {
   const options = {
     behavior: 'smooth',
     block: 'start',
     inline: 'nearest',
-  };
+  }
   if (projectContainer.className.includes('hidden')) {
-    projectContainer.setAttribute('class', 'fade-in-element');
-  };
-  projectContainer.scrollIntoView(options);
-});
+    projectContainer.setAttribute('class', 'fade-in-element')
+  }
+  projectContainer.scrollIntoView(options)
+})
+
+// glitch logic
+const welcomeTextCont = document.getElementById('welcome-text-container')
+const name = document.getElementById('name')
+
+welcomeTextCont.addEventListener('mouseover', () => {
+  toggleGlitch()
+})
+welcomeTextCont.addEventListener('mouseleave', () => {
+  toggleGlitch('reset')
+})
+
+const limit = (func, ...args) => {
+  let count = 0
+  return (reset) => {
+    if (reset) {
+      count = 0
+      func(...args, reset)
+    } else if (!count) {
+      func(...args)
+      count += 1
+    }
+  }
+}
+
+const toggleClass = (ele, className, delay, reset) => {
+  if (reset) {
+    setTimeout(() => ele.classList.remove(className), delay)
+  } else {
+    ele.setAttribute('class', className)
+  }
+}
+
+const toggleGlitch = limit(toggleClass, name, 'glitch', 2400)
+const runGlitch = () => {
+  toggleGlitch()
+  toggleGlitch('reset')
+}
+
+const randomInterval = (func, min, max, ...args) => {
+  func(...args)
+  setTimeout(() => randomInterval(func, min, max, ...args), genRand(min, max))
+}
+
+const genRand = (min, max) => Math.floor(Math.random() * max) + min
